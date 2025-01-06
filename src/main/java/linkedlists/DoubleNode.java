fc2454bf5c2c4ad173c016ea92f8e562797cdb25
Sujan Lam <sujann42@gmail.com>
2025-01-06 15:06:43 -0600

    create nodes for both single and doubly linkedlist

---
+++ src/main/java/linkedlists/DoubleNode.java
@@ -0,0 +1,14 @@
+package linkedlists;
+
+public class DoubleNode {
+
+    int data;
+    DoubleNode next;
+    DoubleNode prev;
+
+    DoubleNode(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+}
---
+++ src/main/java/linkedlists/SingleNode.java
@@ -0,0 +1,11 @@
+package linkedlists;
+
+public class SingleNode {
+    int data;
+    SingleNode next;
+
+    SingleNode(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
